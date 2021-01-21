93800202e018b8ef4d87f6928d62b89b40992c17
Leonid Adolph Guadalupe <leonidguadalupe@gmail.com>
2021-01-21 14:38:00 +0800

    Flake 8 lints. Sync view fix on static inputs (now fetches from env). Code cleanup

--- api/admin.py
+++ api/admin.py
@@ -1,11 +1,10 @@
 from django.contrib import admin
-from django.contrib.auth import get_user_model
-from django.contrib.auth.admin import UserAdmin
-
 from api.models import Sync
+
 
 class SyncAdmin(admin.ModelAdmin):
     list_display = ('created_date', 'status', 'reason')
     ordering = ('-created_date',)
+
 
 admin.site.register(Sync, SyncAdmin)
--- api/models/sync.py
+++ api/models/sync.py
@@ -1,7 +1,7 @@
 from django.db import models
-from django.utils import timezone
 
 from .base import BaseModel
+
 
 class Sync(BaseModel):
     REQUESTED = 'requested'
@@ -16,13 +16,15 @@ class Sync(BaseModel):
           ('failed', FAILED)
     )
 
-    status = models.CharField(choices=STATUS_TYPES, default=SYNCING, max_length=64)
+    status = models.CharField(choices=STATUS_TYPES,
+                              default=SYNCING, max_length=64)
     # this field is for adding a reason why syncing failed
     reason = models.CharField(blank=True, null=True, max_length=500)
-    ## TODO:
-    # 1. Implement recording of which user ran sync (add a model foreign key here)
-    # 2. status default will be requested and there will be queueing for syncing action.
-    
+    # TODO:
+    # 1. Implement recording of which user ran sync
+    #    (add a model foreign key here)
+    # 2. status default will be requested and
+    #    there will be queueing for syncing action.
 
     class Meta:
         verbose_name = "sync"
